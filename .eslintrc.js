3102963f18ce908603d71f77fa9df64f10158f13
marchik88 <marchik88@rambler.ru>
2019-09-15 14:30:16 +0300

    chore: update eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,68 @@
-module.exports =  {
-  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
-  extends:  [
-    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+module.exports = {
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
   ],
-  parserOptions:  {
-    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
-    sourceType:  'module',  // Allows for the use of imports
+  rules: {
+    "react/jsx-closing-bracket-location": [
+      0,
+      {
+        selfClosing: "tag-aligned",
+        nonEmpty: "after-props"
+      }
+    ],
+    "react/require-default-props": 0,
+    "global-require": 0,
+    "no-restricted-globals": 0,
+    "no-unused-expressions": 0,
+    "react/jsx-indent": 0 /* conflict with prettier */ ,
+    "react/jsx-wrap-multilines": 0 /* conflict with prettier */ ,
+    "react/jsx-one-expression-per-line": 0 /* conflict with prettier */ ,
+    "no-unused-vars": 0,
+    "jsx-a11y/label-has-for": [
+      2,
+      {
+        required: {
+          some: ["nesting", "id"]
+        }
+      }
+    ],
+    // this does not work with nextjs Link, switching to warn to have it highlighted just in case of any otehr issues
+    "jsx-a11y/anchor-is-valid": "warn",
+    "flowtype/no-types-missing-file-annotation": 0,
+    "import/no-extraneous-dependencies": [
+      0,
+      {
+        devDependencies: [".storybook/**", "stories/**"]
+      }
+    ],
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/ban-ts-ignore": "warn",
+    "@typescript-eslint/interface-name-prefix": 0
   },
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src", "stories"],
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".stories.tsx"]
+      }
+    }
+  },
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module" // Allows for the use of imports
+  },
+  env: {
+    browser: true,
+    jest: true
+  },
+  globals: {
+    fetch: false,
+    require: true,
+    __DEV__: true,
+    window: true,
+    FormData: true
+  }
 };
